83949bf4a92e33da2e34f511ffad75ff27b3175f
CrimeMoot <wakeafa@gmail.com>
2025-01-04 15:37:57 +0500

    fix?

--- Content.Shared/Nutrition/Components/HungerComponent.cs
+++ Content.Shared/Nutrition/Components/HungerComponent.cs
@@ -10,7 +10,7 @@ using Robust.Shared.Serialization.TypeSerializers.Implementations.Generic;
 namespace Content.Shared.Nutrition.Components;
 
 [RegisterComponent, NetworkedComponent, Access(typeof(HungerSystem))]
-[AutoGenerateComponentState(fieldDeltas: true), AutoGenerateComponentPause]
+[AutoGenerateComponentState, AutoGenerateComponentPause]
 public sealed partial class HungerComponent : Component
 {
     /// <summary>
